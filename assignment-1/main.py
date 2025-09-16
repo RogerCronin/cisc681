eac0af6a4fd88fa88b0ce4ea4f9d74bea4335510
Roger <57421158+RogerCronin@users.noreply.github.com>
2025-09-16 18:28:14 -0400

    working, just need to speed it up now...

--- assignment-1/main.py
+++ assignment-1/main.py
@@ -1,7 +1,17 @@
 # written for Python 3.13.7
 
-from tests import base_tests, problem_1_tests, problem_2_tests, problem_3_tests
 import sys
+from collections.abc import Callable
+
+from switch import Yard, State, Action
+from tests import base_tests, problem_1_tests, problem_2_tests, problem_3_tests
+from search import blind_tree_search
+from data import \
+    yard_1, init_state_1, goal_state_1, \
+    yard_2, init_state_2, goal_state_2, \
+    yard_3, init_state_3, goal_state_3, \
+    yard_4, init_state_4, goal_state_4, \
+    yard_5, init_state_5, goal_state_5
 
 def print_help():
     print("Usage:")
@@ -10,11 +20,29 @@ def print_help():
     print("  python main.py heuristic <yard>")
     print("  python main.py graph <yard>")
     print()
-    print("Tests: base, test1, test2, test3")
+    print("Tests: base, test1, test2, test3, full")
     print("Yards: YARD-1, YARD-2, YARD-3, YARD-4, YARD-5")
     print()
     print("Please check the writeup.pdf attached with this assignment's Canvas submission.")
     print("Run with Python 3.13.7!")
+
+def execute_search(search: Callable[[Yard, State, State], list[Action]], yard_name: str):
+    match yard_name:
+        case "YARD-1" | "yard1" | "yard-1" | "yard_1":
+            result = search(yard_1, init_state_1, goal_state_1)
+        case "YARD-2" | "yard2" | "yard-2" | "yard_2":
+            result = search(yard_2, init_state_2, goal_state_2)
+        case "YARD-3" | "yard3" | "yard-3" | "yard_3":
+            result = search(yard_3, init_state_3, goal_state_3)
+        case "YARD-4" | "yard4" | "yard-4" | "yard_4":
+            result = search(yard_4, init_state_4, goal_state_4)
+        case "YARD-5" | "yard5" | "yard-5" | "yard_5":
+            result = search(yard_5, init_state_5, goal_state_5)
+        case _:
+            print_help()
+            return
+    
+    print(result)
 
 def main():
     args = sys.argv[1:]
@@ -29,13 +57,18 @@ def main():
             problem_2_tests()
         elif args[0] == "test3":
             problem_3_tests()
+        elif args[0] == "full":
+            base_tests()
+            problem_1_tests()
+            problem_2_tests()
+            problem_3_tests()
         else:
             print_help()
             return
         print("Tests passed!")
     elif n == 2:
         if args[0] == "blind":
-            ...
+            execute_search(blind_tree_search, args[1])
         elif args[0] == "heuristic":
             ...
         elif args[0] == "graph":
--- assignment-1/search.py
+++ assignment-1/search.py
@@ -1,36 +1,43 @@
+from __future__ import annotations
+
 from collections import deque
 
 from switch import Yard, State, Action, expand_with_actions
 
 class Node:
-    def __init__(self, state: State):
-        self.__init__(state, None, None, 0)
-
-    def __init__(self, state: State, parent_state: State, action: Action, depth: int):
+    def __init__(self, state: State, parent_node: Node = None, action: Action = None, depth: int = 0):
         self.state = state
-        self.parent_state = parent_state
+        self.parent_node = parent_node
         self.action = action
         self.depth = depth
 
 def dfs(yard: Yard, initial_state: State, goal_state: State, depth_limit: int) -> Node:
-    depth = 0
-    fringe = deque(Node(initial_state))
+    fringe = deque([Node(initial_state)])
     while len(fringe) != 0:
         node = fringe.pop()
-        if node == goal_state:
+        if node.state == goal_state:
             return node
         elif node.depth > depth_limit:
             continue
-        for state, action in expand_with_actions(node.state, yard):
-            fringe.append(Node(state, node, action, depth + 1))
+        next_actions = expand_with_actions(node.state, yard)
+        for state, action in next_actions:
+            fringe.append(Node(state, node, action, node.depth + 1))
     return None
 
 # we use iterative deepening (see PDF writeup for further details)
 def blind_tree_search(yard: Yard, initial_state: State, goal_state: State) -> list[Action]:
     depth_limit = 0
     while True:
+        print(f"Checking with depth_limit = {depth_limit}")
         result = dfs(yard, initial_state, goal_state, depth_limit)
         if result:
             break
         depth_limit += 1
-    print(result)
+
+    # we found a result! backtrack actions through the tree now
+    actions = []
+    while result.parent_node:
+        actions.insert(0, result.action) # put at front
+        result = result.parent_node
+    
+    return actions
