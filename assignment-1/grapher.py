2440307a9e46b12b8fc4ad26813cfde5ac2177e0
Roger Cronin <roger.m.cronin@gmail.com>
2025-09-18 08:32:02 -0400

    add runtime stats

---
+++ assignment-1/grapher.py
@@ -0,0 +1,2 @@
+# I use this to generate reports for my writeup using manually collected data
+
--- assignment-1/search.py
+++ assignment-1/search.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from heapq import heappush, heappop
 from collections import deque
 from typing import Callable
+from time import perf_counter
 
 from switch import Yard, State, Action, expand_with_actions
 
@@ -24,25 +25,27 @@ def backtrack_actions_through_tree(node: Node) -> list[Action]:
         node = node.parent_node
     return actions
 
-def dfs(yard: Yard, initial_state: State, goal_state: State, depth_limit: int) -> Node | None:
+def dfs(yard: Yard, initial_state: State, goal_state: State, depth_limit: int) -> tuple[Node | None, int]:
+    nodes_expanded = 1
     fringe = deque([Node(initial_state)]) # we use a deque instead of a list cause it's faster
     while len(fringe) != 0: # while we still have states to process,
         node = fringe.pop()
         if node.state == goal_state: # if the current state is a goal state, we're done!
-            return node
+            return (node, nodes_expanded)
         elif node.depth > depth_limit: # if the current state is too deep, ignore
             continue
         next_actions = expand_with_actions(node.state, yard) # otherwise get the next states we can go to
+        nodes_expanded += 1
         for state, action in next_actions: # add them to the fringe, increasing the depth by 1
             fringe.append(Node(state, node, action, node.depth + 1))
-    return None # if we exhausted the fringe, we couldn't find a solution...
+    return (None, nodes_expanded) # if we exhausted the fringe, we couldn't find a solution...
 
 # my heuristic is total number of cars - number of cars in the goal state
 # this function generates a valid f(n) = g(n) + h(n) with... uh... monads?
 def f_factory(number_of_cars: int, end_track: int) -> Callable[[Node], int]:
     return lambda node: node.depth + (number_of_cars - node.state.number_of_cars_on_track(end_track))
 
-def dijkstras(yard: Yard, initial_state: State, goal_state: State) -> Node | None:
+def dijkstras(yard: Yard, initial_state: State, goal_state: State) -> tuple[Node | None, int]:
     # make our f(n) cost function
     number_of_cars = initial_state.count_number_of_cars()
     end_track = goal_state.get_track_with_engine()
@@ -53,20 +56,22 @@ def dijkstras(yard: Yard, initial_state: State, goal_state: State) -> Node | Non
         def __lt__(self, other):
             return f(self) < f(other)
 
+    nodes_expanded = 1
     root_node = ComparisonNode(initial_state)
     fringe = []
     heappush(fringe, root_node) # make a heap and push our initial state node to it
     while len(fringe) != 0: # while we still have states to process,
         node = heappop(fringe)
         if node.state == goal_state: # if the current state is a goal state, we're done!
-            return node
+            return (node, nodes_expanded)
         next_actions = expand_with_actions(node.state, yard) # otherwise get the next states we can go to
+        nodes_expanded += 1
         for state, action in next_actions:
             child_node = ComparisonNode(state, node, action, node.depth + 1)
             heappush(fringe, child_node) # add them to the fringe, increasing the depth by 1
-    return None # if we exhausted the fringe, we couldn't find a solution...
+    return (None, nodes_expanded) # if we exhausted the fringe, we couldn't find a solution...
 
-def graph_search(yard: Yard, initial_state: State, goal_state: State) -> Node | None:
+def graph_search(yard: Yard, initial_state: State, goal_state: State) -> tuple[Node | None, int]:
     # this is almost identical to dijkstras except we keep track of visited states with a set
     number_of_cars = initial_state.count_number_of_cars()
     end_track = goal_state.get_track_with_engine()
@@ -77,49 +82,61 @@ def graph_search(yard: Yard, initial_state: State, goal_state: State) -> Node | 
             return f(self) < f(other)
     
     closed = set() # keeps a list of states so we don't accidentally backtrack in the search graph
+    nodes_expanded = 1
     root_node = ComparisonNode(initial_state)
     fringe = []
     heappush(fringe, root_node)
     while len(fringe) != 0:
         node = heappop(fringe)
         if node.state == goal_state:
-            return node
+            return (node, nodes_expanded)
         state_representation = str(node.state) # we have to use this intermediary representation cause python can't hash dicts
         if not state_representation in closed:
             closed.add(state_representation) # if we're checking out a new state, add it to the closed set
             next_actions = expand_with_actions(node.state, yard)
+            nodes_expanded += 1
             for state, action in next_actions:
                 child_node = ComparisonNode(state, node, action, node.depth + 1)
                 heappush(fringe, child_node)
-    return None
+    return (None, nodes_expanded)
 
 # we use iterative deepening (see PDF writeup for further details)
-def blind_tree_search(yard: Yard, initial_state: State, goal_state: State) -> list[Action]:
+def blind_tree_search(yard: Yard, initial_state: State, goal_state: State, report_depth = True) -> list[Action]:
     depth_limit = 0
+
+    start_time = perf_counter()
     while True: # forever,
-        print(f"Checking with depth limit {depth_limit + 1}...")
-        result = dfs(yard, initial_state, goal_state, depth_limit) # do dfs at this depth
+        if report_depth:
+            print(f"Checking with depth limit {depth_limit + 1}...")
+        result, nodes_expanded = dfs(yard, initial_state, goal_state, depth_limit) # do dfs at this depth
         if result: # if we found a result, yay!
             break
         depth_limit += 1 # otherwise increase the depth by one and retry
+    end_time = perf_counter()
     
-    print("Found a solution!")
+    print(f"Found a solution with {nodes_expanded} expansions taking {round(end_time - start_time, 3)} seconds!")
     return backtrack_actions_through_tree(result)
 
 # I think heuristic_tree_search and heuristic_graph_search are pretty self explanatory, no?
 
 def heuristic_tree_search(yard: Yard, initial_state: State, goal_state: State) -> list[Action]:
-    result = dijkstras(yard, initial_state, goal_state)
+    start_time = perf_counter()
+    result, nodes_expanded = dijkstras(yard, initial_state, goal_state)
+    end_time = perf_counter()
+
     if not result:
         raise Exception("A* tree search failed to find a path")
     
-    print("Found a solution!")
+    print(f"Found a solution with {nodes_expanded} expansions taking {round(end_time - start_time, 3)} seconds!")
     return backtrack_actions_through_tree(result)
 
 def heuristic_graph_search(yard: Yard, initial_state: State, goal_state: State) -> list[Action]:
-    result = graph_search(yard, initial_state, goal_state)
+    start_time = perf_counter()
+    result, nodes_expaned = graph_search(yard, initial_state, goal_state)
+    end_time = perf_counter()
+
     if not result:
         raise Exception("A* graph search failed to find a path")
 
-    print("Found a solution!")
+    print(f"Found a solution with {nodes_expaned} expansions taking {round(end_time - start_time, 3)} seconds!")
     return backtrack_actions_through_tree(result)
